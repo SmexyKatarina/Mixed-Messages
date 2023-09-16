5f8089b79fb145e9e7a912c5948bc8e44fd3b38b
Kat <mercier200012@hotmail.com>
2023-09-16 15:57:01 -0400

    Added file and general idea

---
+++ main.js
@@ -0,0 +1,7 @@
+/*
+General Idea of the code
+- Get messages that perform well together / multiple messages to make it sound cool.
+- Randomize the messages and return it in an array.
+- 3 pieces of messages.
+- What to differently?
+*/
