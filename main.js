08c051304fa90aafc939eefd597d5276962a72da
Kat <mercier200012@hotmail.com>
2023-09-16 16:36:51 -0400

    Installed 'Prompt-Sync' mddule Added new start method Added generation functions Added some extra arrays for different responses

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ General Idea of the code
 - What to differently?
 */
 
+const prompt = require('prompt-sync')({sigint: true});
+
+const responses = [
+    'Great!',
+    'Coolio!',
+    'Alrighty!',
+    'Awesome!'
+];
+
 const puns = [
     'What did one blade of grass say to another about the lack of rain?', 
     'What lies at the bottom of the ocean and twitches?', 
@@ -30,7 +39,39 @@ const punchLine = [
     'It had too many sleepless knights.'
 ];
 
+const disappointments = [
+    'Aww...',
+    'Why not?',
+    "It'll be funny come on",
+    'Ok I guess...'
+];
+
 const rand = [Math.floor(Math.random() * puns.length), Math.floor(Math.random() * throwbackAnswers.length)];
 rand.push(rand[0]);
 
-console.log(`${puns[rand[0]]}\n${throwbackAnswers[rand[1]]}\n${punchLine[rand[0]]}`);
+const generateNumber = (max) => Math.floor(Math.random * max);
+
+const getRandomElement = (array) => array[generateNumber(array.length)];
+
+const start = () => 
+{
+    let repeat = 0;
+    while (repeat < 10) 
+    {
+        const hearJoke = prompt('Do you want to hear a joke?');
+        switch (hearJoke) 
+        {
+            case 'yes':
+            case 'y':
+                console.log(`${getRandomElement(responses)}\n${getRandomElement(puns)}\n${getRandomElement(throwbackAnswers)}\n${getRandomElement(punchLine)}`);
+                break;
+            case 'no':
+            case 'n':
+                console.log(getRandomElement(disappointments));
+                break;
+            default:
+                console.log(`${hearJoke} isn't a possible input. Acceptable inputs are: yes/y and no/n.`);
+                break;
+        }
+    }
+}
